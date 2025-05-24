25e471b533cd3f6eba59659ef8672f44391ba351
sniperkla <sniperkla@gmail.com>
2025-05-24 16:54:02 +0700

    testtong

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 const express = require('express')
 const HTTPStatus = require('http-status')
 const app = express()
-const port = 3002
+const port = 9999
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const url = require('./lib/combineUser')
-const axios = require('axios')
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/gettrading', async (req, res) => {
+app.post('/getUser', async (req, res) => {
   try {
     const bodyq = req.body
-    const urls = url.combineUser()
-    // Use Promise.all to handle multiple requests concurrently
-    await Promise.all(
-      urls.URL.map(async (url) => {
-        await multiUser(url, bodyq)
-      })
-    )
-
-    console.log('Data sent successfully to all URLs:', urls.URL)
-    await delay(5000)
+    console.log('Data sent successfully to all URLs:', req.body)
     return res.status(HTTPStatus.OK).json({ success: true, data: 'success' })
   } catch (error) {
     console.error('Error broadcasting data:', error)
@@ -38,19 +26,3 @@ app.post('/gettrading', async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-const multiUser = async (URL, body) => {
-  try {
-    const response = await axios.post(URL, body)
-    if (response.status === 200 || response.status === 201) {
-      console.log('Data sent successfully to:', URL)
-      // Handle successful response data (e.g., process response.data)
-    } else {
-      console.error('Error sending data to:', URL, 'Status:', response.status)
-      // Handle unsuccessful response (e.g., display error message to user)
-    }
-  } catch (error) {
-    console.error('Error sending data to:', URL, 'Error:', error)
-    // Handle errors (e.g., network issues, server errors)
-  }
-}
